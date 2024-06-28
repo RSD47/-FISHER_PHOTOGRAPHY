e32041043f86d75370a4bd6ec0f73011025d3819
Samuel Anderson <samuelSD.999@gmail.com>
2024-06-28 23:29:59 +0100

    Added actions to photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,49 @@
 class PhotosController < ApplicationController
+
+  def index
+    @photos = Photo.all
+  end
+
+  def show
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+
+    if @photo.save
+      redirect_to photos_path, notice: "Photos Added"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @photo.update(photo_params)
+      redirect_to photos_path, notice: "Task updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @photo.destroy
+    redirect_to photos_path, status: :see_other, notice: "Photos deleted"
+  end
+
+  private
+
+  def set_photo
+    @photo = Photo.find(params[:id])
+  end
+
+  def photo_params
+    params.require(:photo).permit(:season, :match)
+  end
 end
