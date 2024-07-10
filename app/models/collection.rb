3ddb66b62d156c2212f919018d510c344698aef5
Samuel Anderson <samuelSD.999@gmail.com>
2024-07-10 03:20:03 +0100

    Added new view for collections and changed title to match in the collections table

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,4 +1,5 @@
 class Collection < ApplicationRecord
+  has_many_attached :photos
   belongs_to :user
   belongs_to :photo
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_10_014535) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_10_021736) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "collections", force: :cascade do |t|
-    t.string "title"
     t.string "season"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "match"
   end
 
   create_table "photos", force: :cascade do |t|
